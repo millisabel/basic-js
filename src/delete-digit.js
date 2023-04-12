c948e14b7ab275102b7e04668190bc5418e58370
millisabel <elena.kozlova.job@gmail.com>
2023-04-13 00:49:00 +0200

    feat: done delete-digit

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Given some integer, find the maximal number you can obtain
  * by deleting exactly one digit of the given number.
@@ -11,10 +9,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  let str = String(n);
+  let arr = [];
+  for (let i = 0; i < str.length; i++) {
+    let x = [...str];
+    x.splice(i, 1);
+    arr.push(+x.join(''));
+  }
+
+  return Math.max(...arr);
+
 }
+
 
 module.exports = {
   deleteDigit
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
@@ -20,15 +18,89 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  charIndex = (str) => str.toUpperCase().split('').map(el => {
+    return this.alphabet.indexOf(el) === -1 ? el : this.alphabet.indexOf(el)
+  })
+
+  encrypt(message, key) {
+    if (typeof message === 'string' && typeof key === 'string') {
+      let encrypted = '';
+
+      let messageNum = this.charIndex(message);
+      let keyNum = this.charIndex(key);
+      if (keyNum.length < messageNum.length) {
+        keyNum = Array(Math.ceil(messageNum.length / keyNum.length)).fill(keyNum).flat();
+      }
+      keyNum.length = messageNum.length;
+      let keySkip = 0;
+
+      for (let i = 0; i < message.length; i++) {
+        if (typeof messageNum[i] === 'string') {
+          encrypted += messageNum[i];
+          keySkip += 1;
+        } else {
+          encrypted += this.alphabet.charAt((messageNum[i] + keyNum[i - keySkip]) % 26)
+        }
+      }
+      if (this.direct) {
+        return encrypted;
+      } else {
+        encrypted = encrypted.split('').reverse().join('')
+        return encrypted
+      }
+
+    } else {
+      throw Error('Incorrect arguments!')
+    }
+  }
+
+  decrypt(message, key) {
+    if (typeof message === 'string' && typeof key === 'string') {
+      let decrypted = '';
+
+      let messageNum = this.charIndex(message);
+      let keyNum = this.charIndex(key);
+      if (keyNum.length < messageNum.length) {
+        keyNum = Array(Math.ceil(messageNum.length / keyNum.length)).fill(keyNum).flat();
+      }
+      keyNum.length = messageNum.length;
+      let keySkip = 0;
+
+      for (let i = 0; i < message.length; i++) {
+        if (typeof messageNum[i] === 'string') {
+          decrypted += messageNum[i];
+          keySkip += 1;
+        } else {
+          if (messageNum[i] - keyNum[i - keySkip] >= 0) {
+            decrypted += this.alphabet.charAt((messageNum[i] - keyNum[i - keySkip]) % 26)
+          } else {
+            decrypted += this.alphabet.charAt((messageNum[i] - keyNum[i - keySkip] + 26) % 26)
+          }
+        }
+      }
+      if (this.direct) {
+        return decrypted;
+      } else {
+        decrypted = decrypted.split('').reverse().join('')
+        return decrypted
+      }
+    } else {
+      throw Error('Incorrect arguments!')
+    }
   }
 }
+
+const directMachine = new VigenereCipheringMachine();
+const reverseMachine = new VigenereCipheringMachine(false);
+directMachine.encrypt('attack at dawn!', 'alphonse');
+// reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse');
+// console.log(directMachine.encrypt('attack at dawn!', 'alphonse'))
+
 
 module.exports = {
   VigenereCipheringMachine
