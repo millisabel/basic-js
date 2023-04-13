e3175e54893e955ee7858efef797c20bf66ef051
millisabel <elena.kozlova.job@gmail.com>
2023-04-13 22:21:26 +0200

    feat: done mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
  * that don't contain a mine have a number in it that indicates the total number of mines
@@ -23,10 +21,56 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function minesweeper(matrix) {
+  let arr = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+
+    let arrInner = [];
+
+    for (let j = 0; j < matrix[i].length; j++) {
+      let count = 0;
+      if(matrix[i][j + 1]){
+        count++;
+      }
+      if(matrix[i][j - 1]){
+        count++;
+      }
+      if(matrix[i-1] && matrix[i-1][j]){
+        count++;
+      }
+      if(matrix[i-1] && matrix[i-1][j - 1]){
+        count++;
+      }
+      if(matrix[i-1] && matrix[i-1][j + 1]){
+        count++;
+      }
+      if(matrix[i+1] && matrix[i+1][j - 1]){
+        count++;
+      }
+      if(matrix[i+1] && matrix[i+1][j + 1]){
+        count++;
+      }
+      if(matrix[i+1] && matrix[i+1][j]){
+        count++;
+      }
+
+      arrInner.push(count);
+    }
+
+    arr.push(arrInner);
+  }
+  return arr;
 }
+
+const matrix = [
+  [true, false, false],
+  [false, true, false],
+  [false, false, false]
+];
+
+console.log(minesweeper(matrix));
 
 module.exports = {
   minesweeper
